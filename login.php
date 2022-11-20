0792eff4fe5e2a99c26c8614b789728014a79a9b
dircksens <marianadircksen13@gmail.com>
2022-11-20 18:33:49 -0300

    .

---
+++ login.php
@@ -0,0 +1,10 @@
+<form method="post">
+    Login: <br>
+    <input type="text" name="login" placeholder="e-mail">
+    <br>
+    Senha:<br>
+    <input type="password" name="senha">
+    <br>
+    <input type="submit" name="autenticar" value="Logar">
+</form>
+  
